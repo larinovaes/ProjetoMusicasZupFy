815d923059db5bccf7fd0501267bee8ef6a0078c
Larissa Novais <larinovais54@gmail.com>
2021-12-10 12:03:07 -0300

    Adicionar conversor para modelMapper

--- src/main/java/br/com/zup/Zupfy/components/Conversor.java
+++ src/main/java/br/com/zup/Zupfy/components/Conversor.java
@@ -1,5 +1,14 @@
-package br.com.zup.Zupfy.modelMapper;
+package br.com.zup.Zupfy.components;
 
-public class ModelMapper {
-    
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Conversor {
+
+    @Bean
+    private ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
