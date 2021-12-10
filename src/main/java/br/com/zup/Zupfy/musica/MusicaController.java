505c543862bf4e10bce092295c35fc4a36add232
Larissa Novais <larinovais54@gmail.com>
2021-12-10 14:20:41 -0300

    Criar metodo para mapear cadastro de musicas

--- src/main/java/br/com/zup/Zupfy/musica/MusicaController.java
+++ src/main/java/br/com/zup/Zupfy/musica/MusicaController.java
@@ -1,14 +1,28 @@
 package br.com.zup.Zupfy.musica;
 
+import br.com.zup.Zupfy.musica.dtos.MusicaDetalhesDTO;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/musicas")
 public class MusicaController {
     @Autowired
     private MusicaService musicaService;
     @Autowired
     private ModelMapper modelMapper;
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    private MusicaDetalhesDTO cadastrarMusica(MusicaDetalhesDTO musicaDetalhesDTO) {
+     Musica musica = modelMapper.map(musicaDetalhesDTO, Musica.class);
+     musicaService.cadastrarMusica(musica);
+     musicaDetalhesDTO = modelMapper.map(musica, MusicaDetalhesDTO.class);
+     return musicaDetalhesDTO;
+    }
 }
