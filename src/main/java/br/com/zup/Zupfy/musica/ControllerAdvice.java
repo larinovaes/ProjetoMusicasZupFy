2cb47318afa87b9815598c53a57213cda9aa60b8
Larissa Novais <larinovais54@gmail.com>
2021-12-10 14:42:02 -0300

    Criar metodo para excessao de validação

--- src/main/java/br/com/zup/Zupfy/musica/ControllerAdvice.java
+++ src/main/java/br/com/zup/Zupfy/musica/ControllerAdvice.java
@@ -1,8 +1,29 @@
 package br.com.zup.Zupfy.musica;
 
+import br.com.zup.Zupfy.musica.exceptions.MensagemDeErro;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestControllerAdvice
 public class ControllerAdvice {
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    private List<MensagemDeErro> tratarExcessaoDeValidacao(MethodArgumentNotValidException exception) {
+        List<MensagemDeErro> erros = new ArrayList<>();
+
+        for (FieldError fieldError : exception.getFieldErrors()) {
+            MensagemDeErro mensagemErro = new MensagemDeErro(fieldError.getDefaultMessage());
+            erros.add(mensagemErro);
+        }
+
+      return erros;
+    }
 }
