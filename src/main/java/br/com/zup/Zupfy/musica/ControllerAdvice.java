0c7d20e9b41a71bd49148220aaa0429465cbe3d9
Larissa Novais <larinovais54@gmail.com>
2021-12-10 14:35:18 -0300

    Criar classe de mensagem de erro

---
+++ src/main/java/br/com/zup/Zupfy/musica/ControllerAdvice.java
@@ -0,0 +1,2 @@
+package br.com.zup.Zupfy.musica;public class ControllerAdvice {
+}
---
+++ src/main/java/br/com/zup/Zupfy/musica/exceptions/MensagemDeErro.java
@@ -0,0 +1,9 @@
+package br.com.zup.Zupfy.musica.exceptions;
+
+public class MensagemDeErro {
+    private String mensagem;
+
+    public MensagemDeErro(String mensagem) {
+        this.mensagem = mensagem;
+    }
+}
